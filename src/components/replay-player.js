19c3e8bb4f9b1369b959e26034321630c2a0c4be
Viktor Radulov <golova@golova.dev>
2021-12-19 10:48:32 +0200

    Fixed player height consideration

--- src/components/replay-player.js
+++ src/components/replay-player.js
@@ -46,25 +46,27 @@ AFRAME.registerComponent('replay-player', {
           let nextFrame = frames[frameIndex != frames.length - 1 ? frameIndex + 1 : frameIndex];
     
           let room = replay.info.room;
+          let height = clamp((replay.info.height - 1.8) * 0.5, -0.2, 0.6);
+          
           
           if (replay.info.leftHanded) {
-            this.leftHandedTock(room, frame, nextFrame);
+            this.leftHandedTock(room, frame, nextFrame, height);
           } else {
-            this.rightHandedTock(room, frame, nextFrame);
+            this.rightHandedTock(room, frame, nextFrame, height);
           }
         }
       },
-    rightHandedTock: function(room, frame, nextFrame) {
+    rightHandedTock: function(room, frame, nextFrame, height) {
           this.saberEls[0].object3D.position.x = frame.l.p.x - room.x;
-          this.saberEls[0].object3D.position.y = frame.l.p.y - room.y;// + (height - 2.0);
+          this.saberEls[0].object3D.position.y = frame.l.p.y - room.y + height;
           this.saberEls[0].object3D.position.z = -frame.l.p.z + room.z;
     
           this.saberEls[1].object3D.position.x = frame.r.p.x - room.x;
-          this.saberEls[1].object3D.position.y = frame.r.p.y - room.y;// + (height - 2.0);
+          this.saberEls[1].object3D.position.y = frame.r.p.y - room.y + height;
           this.saberEls[1].object3D.position.z = -frame.r.p.z + room.z;
 
           this.headset.object3D.position.x = frame.h.p.x - room.x;
-          this.headset.object3D.position.y = frame.h.p.y - room.y;
+          this.headset.object3D.position.y = frame.h.p.y - room.y + height;
           this.headset.object3D.position.z = -frame.h.p.z + room.z;
     
           var lquat = new THREE.Quaternion(frame.l.r.w, frame.l.r.z, frame.l.r.y, frame.l.r.x).slerp(new THREE.Quaternion(nextFrame.l.r.w, nextFrame.l.r.z, nextFrame.l.r.y, nextFrame.l.r.x), 0.2);
@@ -88,17 +90,17 @@ AFRAME.registerComponent('replay-player', {
           this.headset.object3D.rotation.y = hrotation.y + Math.PI;
           this.headset.object3D.rotation.z = hrotation.z + Math.PI;;
     },
-    leftHandedTock: function(room, frame, nextFrame) {
+    leftHandedTock: function(room, frame, nextFrame, height) {
           this.saberEls[0].object3D.position.x = -frame.r.p.x + room.x;
-          this.saberEls[0].object3D.position.y = frame.r.p.y - room.y;// + (height - 2.0);
+          this.saberEls[0].object3D.position.y = frame.r.p.y - room.y + height;
           this.saberEls[0].object3D.position.z = -frame.r.p.z + room.z;
     
           this.saberEls[1].object3D.position.x = -frame.l.p.x + room.x;
-          this.saberEls[1].object3D.position.y = frame.l.p.y - room.y;// + (height - 2.0);
+          this.saberEls[1].object3D.position.y = frame.l.p.y - room.y + height;
           this.saberEls[1].object3D.position.z = -frame.l.p.z + room.z;
 
           this.headset.object3D.position.x = -frame.h.p.x + room.x;
-          this.headset.object3D.position.y = frame.h.p.y - room.y;
+          this.headset.object3D.position.y = frame.h.p.y - room.y + height;
           this.headset.object3D.position.z = -frame.h.p.z + room.z;
     
           var rquat = new THREE.Quaternion(frame.r.r.w, -frame.r.r.z, -frame.r.r.y, frame.r.r.x).slerp(new THREE.Quaternion(nextFrame.r.r.w, -nextFrame.r.r.z, -nextFrame.r.r.y, nextFrame.r.r.x), 0.2);
@@ -123,3 +125,5 @@ AFRAME.registerComponent('replay-player', {
           this.headset.object3D.rotation.z = hrotation.z + Math.PI;;
     },
 });
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);

6caf49b18b3737e5311a90f2976f1638d9755c96
NSGolova <golova@golova.dev>
2022-05-05 00:04:05 +0100

    Fixed even more unicode shenanigans

--- src/open-replay-decoder.js
+++ src/open-replay-decoder.js
@@ -318,7 +318,7 @@ function DecodeUint8(dataView)
 function DecodeString(dataView)
 {
     const length = dataView.getInt32(dataView.pointer, true);
-    if (length > 1000) { 
+    if (length < 0 || length > 1000) { 
         dataView.pointer += 1;
         return DecodeString(dataView);
      }

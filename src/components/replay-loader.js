382754df87dfc8c0671894f079f5e72463384b82
NSGolova <golova@golova.dev>
2022-09-04 19:03:14 +0100

    Sort by index is no more needed
    (cherry picked from commit 27357bf91ff955d9efe59216a4fbfce20b912aa3)

--- src/components/replay-loader.js
+++ src/components/replay-loader.js
@@ -357,8 +357,6 @@ AFRAME.registerComponent('replay-loader', {
 		allStructs.sort(function (a, b) {
 			if (a.time < b.time) return -1;
 			if (a.time > b.time) return 1;
-			if (a.index < b.index) return -1;
-			if (a.index > b.index) return 1;
 			return 0;
 		});
 

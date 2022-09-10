1dce7c75a488123bc61ffc33892cfc2b04c01b89
NSGolova <golova@golova.dev>
2022-09-10 01:26:55 +0100

    Fixed replays with duplicated miss events

--- src/components/replay-loader.js
+++ src/components/replay-loader.js
@@ -306,13 +306,16 @@ AFRAME.registerComponent('replay-loader', {
 			wallStructs.push(note);
 		}
 
-		var group, groupIndex, groupTime;
+		var group,
+			groupIndex,
+			groupTime,
+			offset = 0;
 
 		const processGroup = () => {
 			for (var j = 0; j < group.length; j++) {
 				const mapnote = mapnotes[group[j]];
 				for (var m = 0; m < group.length; m++) {
-					const replaynote = noteStructs[groupIndex + m];
+					const replaynote = noteStructs[groupIndex + offset + m];
 
 					const lineIndex = mapnote._lineIndex;
 					const colorType = mapnote._type;
@@ -337,6 +340,11 @@ AFRAME.registerComponent('replay-loader', {
 
 		for (var i = 0; i < mapnotes.length && i < noteStructs.length; i++) {
 			if (!group) {
+				if (i > 0 && noteStructs[i + offset].spawnTime == noteStructs[i + offset - 1].spawnTime) {
+					offset++;
+					i--;
+					continue;
+				}
 				group = [i];
 				groupIndex = i;
 				groupTime = mapnotes[i]._time;
